8fdbdc8ba289302b00f62d27ea582e7ba232810d
NozomiTachizawa <mugikko27@gmail.com>
2022-06-27 18:09:34 +0900

    基礎実装

---
+++ database/migrations/2022_06_27_075609_create_reviews_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 50);
+            $table->string('brand', 20);
+            $table->integer('price');
+            $table->string('place', 20);
+            $table->string('body', 400);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
